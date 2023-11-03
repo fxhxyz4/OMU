ab375830a522e07156a3f65169e5d3c1301aa7be
fxhxyz4 <fxhxyz@proton.me>
2023-11-03 22:21:21 +0200

    Update index.js

--- www/pages/index.js
+++ www/pages/index.js
@@ -47,36 +47,38 @@ const Home = () => {
         <link href="https://unpkg.com/modern-normalize@2.0.0/modern-normalize.css" rel="stylesheet" />
 				<title>OMU</title>
 			</Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href='https://github.com/fxhxyz4/OMU' target={'blank'} rel={'noopener, noreferrer'}>
-            OMU
-          </a>
-        </h1>
-        <h2 className={styles.download}>Download for {browser}</h2>
-        <div className={styles.browsers}>
-          <ul className={styles.list}>
-          {browser && (
-              <li className={styles.item}>
-                <a className={styles.fontbrands} href={browserLinks[browser]} target={'blank'} rel={'noopener, noreferrer'}>
-                  {browser === 'Firefox' && <FontAwesomeIcon icon={faFirefox} alt="Firefox icon" title='Firefox' />}
-                  {browser === 'Chrome' && <FontAwesomeIcon icon={faChrome} alt="Chrome icon" title='Google Chrome' />}
-                  {browser === 'Opera' && <FontAwesomeIcon icon={faOpera} alt="Opera icon" title='Opera' />}
-                  {browser === 'Edge' && <FontAwesomeIcon icon={faEdge} alt="Ms Edge icon" title='Microsoft Edge' />}
-                </a>
-              </li>
-            )}
-            <label htmlFor="browserSelect" className={styles.hidden}>Choose a browser:</label>
-            <select id="browserSelect" className={styles.select} value={browser} onChange={(e) => setBrowser(e.target.value)}>
-              <option value="" disabled>Select a browser</option>
-              <option value="Firefox">Mozilla Firefox</option>
-              <option value="Chrome">Google Chrome</option>
-              <option value="Opera">Opera</option>
-              <option value="Edge">Microsoft Edge</option>
-            </select>
-					</ul>
-        </div>
-      </main>
+      <div className={`${animate__animated} ${animate__fadeIn} ${animate__slower}`}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            <a href='https://github.com/fxhxyz4/OMU' target={'blank'} rel={'noopener, noreferrer'}>
+              OMU
+            </a>
+          </h1>
+          <h2 className={styles.download}>Download for {browser}</h2>
+          <div className={styles.browsers}>
+            <ul className={styles.list}>
+            {browser && (
+                <li className={styles.item}>
+                  <a className={styles.fontbrands} href={browserLinks[browser]} target={'blank'} rel={'noopener, noreferrer'}>
+                    {browser === 'Firefox' && <FontAwesomeIcon icon={faFirefox} alt="Firefox icon" title='Firefox' />}
+                    {browser === 'Chrome' && <FontAwesomeIcon icon={faChrome} alt="Chrome icon" title='Google Chrome' />}
+                    {browser === 'Opera' && <FontAwesomeIcon icon={faOpera} alt="Opera icon" title='Opera' />}
+                    {browser === 'Edge' && <FontAwesomeIcon icon={faEdge} alt="Ms Edge icon" title='Microsoft Edge' />}
+                  </a>
+                </li>
+              )}
+              <label htmlFor="browserSelect" className={styles.hidden}>Choose a browser:</label>
+              <select id="browserSelect" className={styles.select} value={browser} onChange={(e) => setBrowser(e.target.value)}>
+                <option value="" disabled>Select a browser</option>
+                <option value="Firefox">Mozilla Firefox</option>
+                <option value="Chrome">Google Chrome</option>
+                <option value="Opera">Opera</option>
+                <option value="Edge">Microsoft Edge</option>
+              </select>
+  					</ul>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
